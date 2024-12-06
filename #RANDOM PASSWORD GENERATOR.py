b86234d8ce49ddced4faeb790ce2139ac14d09a8
AlphaTanishq <124170047+AlphaTanishq@users.noreply.github.com>
2024-12-06 22:29:40 +0530

    #RANDOM PASSWORD GENERATOR.py

--- #RANDOM PASSWORD GENERATOR.py
+++ #RANDOM PASSWORD GENERATOR.py
@@ -7,7 +7,11 @@ alphaS='abcdefghijklmnopqrstuvwxyz'
 num='1234567890'
 smybl='@#$._'
 
-length=int(input("Enter length of password (max 17) : "))
+length=int(input("Enter length of password (b/w 4 to 17) : "))
+if length<4 or length>17:
+    print("\nEnter valid password length")
+    exit()
+
 is_num=input("Wanna include numbers? (y/n) : ")
 is_smybl=input("Wanna include symbols? (y/n) : ")
 freq=[0,0,0,0]
@@ -15,7 +19,17 @@ freq_element=4
 pass0 = ''
 passL = ''
 
+
 def freq_decider(length,freq,freq_element,num):
+    if is_num=='y' and is_smybl!='y':
+        freq_element=3
+    elif is_num!='y' and is_smybl=='y':
+        freq_element=3
+    elif is_num!='y' and is_smybl!='y':
+        freq_element=2
+    else:
+        pass
+
     while length!=sum(freq):
         freq = random.choices(num[:9],k=freq_element)
         for i in freq:
@@ -24,11 +38,11 @@ def freq_decider(length,freq,freq_element,num):
 #freq=freq_decider(length,freq,freq_element,num)                #  [ save freq like this ]
 freq=freq_decider(length,freq,freq_element,num)
 
+
 def pass_gen(pass0,passL):
     try:
         for i in range(freq[0]):
             pass0 += random.choice(alphaC)
-        
         for i in range(freq[1]):
             pass0 += random.choice(alphaS)
         for i in range(freq[2]):
@@ -58,6 +72,9 @@ def main(freq,pass0):
         print("Your password is : ",pass0)
     elif is_num!='y' and is_smybl=='y':
         freq.insert(2,0)
+        pass0 = pass_gen(pass0,passL)
+        print("\nYour password is : ",pass0)
+    elif is_num!='y' and is_smybl!='y':
         pass0 = pass_gen(pass0,passL)
         print("\nYour password is : ",pass0)
     else:
